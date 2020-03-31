54014d9b50dbc35cd3f42cacf267ac6523a0177b
Hailey Somerville <hailey@hailey.lol>
2020-03-31 18:55:05 +1100

    hook up draggable windows

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,19 +1,50 @@
+mod workspace;
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+use workspace::Workspace;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen]
     fn alert(s: &str);
 }
 
+struct App {
+    link: ComponentLink<Self>,
+}
+
+enum Msg {
+    // Click,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        App {
+            link,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <Workspace />
+        }
+    }
+}
+
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, frontend!");
+pub fn start() {
+    yew::start_app::<App>();
 }
