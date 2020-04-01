c8aa63aed35d99684d291c9033500c8dc1d06e14
Hailey Somerville <hailey@hailey.lol>
2020-04-01 23:37:31 +1100

    fix up some event handling stuff

---
+++ frontend/src/util.rs
@@ -0,0 +1,35 @@
+use yew::{Component, ComponentLink, Callback};
+use web_sys::Event;
+
+pub fn stop_propagation<In>() -> Callback<In>
+where
+    In: AsRef<Event>
+{
+    Callback::from(|ev: In| {
+        ev.as_ref().stop_propagation();
+    })
+}
+
+pub fn prevent_default<In>() -> Callback<In>
+where
+    In: AsRef<Event>
+{
+    Callback::from(|ev: In| {
+        ev.as_ref().stop_propagation();
+        ev.as_ref().prevent_default();
+    })
+}
+
+pub fn callback_ex<Comp, F, In, M>(link: &ComponentLink<Comp>, f: F) -> Callback<In>
+where
+    Comp: Component,
+    M: Into<Comp::Message>,
+    F: Fn(In) -> M + 'static,
+    In: AsRef<Event>
+{
+    link.callback(move |ev: In| {
+        ev.as_ref().stop_propagation();
+        ev.as_ref().prevent_default();
+        f(ev)
+    })
+}
