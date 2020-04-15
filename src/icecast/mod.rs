89a538e4fc0bc3e851ad5492dcd6e225e7f8290d
Charlie Somerville <charlie@charlie.bz>
2020-04-15 22:47:03 +1000

    move throttling to source mod

--- src/icecast/mod.rs
+++ src/icecast/mod.rs
@@ -3,7 +3,6 @@ pub mod http;
 use std::fmt::Debug;
 use std::io::{self, Read};
 use std::thread;
-use std::time::{Instant, Duration};
 
 use derive_more::From;
 use tokio::io::AsyncWriteExt;
@@ -94,9 +93,6 @@ fn run_decode_thread(mut send: SourceSend, stream: impl io::Read, content_type: 
         return Ok(());
     }
 
-    let start = Instant::now();
-    let mut total_samples = 0u64;
-
     while let Some(packet) = audio.read().transpose() {
         match packet {
             Ok(StreamRead::Audio(pcm)) => {
@@ -121,28 +117,8 @@ fn run_decode_thread(mut send: SourceSend, stream: impl io::Read, content_type: 
                     }
                 }
 
-                // we intentionally ignore the return value of write indicating
-                // how many samples were written to the ring buffer. if it's
-                // ever less than the number of samples on hand, the receive
-                // side is suffering from serious lag and the best we can do
-                // is drop the data
-
-                let _ = send.write(&samples)
+                send.write(&samples)
                     .map_err(|()| DecodeThreadError::ListenerDisconnected)?;
-
-                // throttle ourselves according to how long these samples should
-                // take to play through. this ensures that fast clients don't
-                // fill the ring buffer up on us
-
-                let elapsed = Duration::from_micros((total_samples * 1_000_000) / SAMPLE_RATE as u64);
-                let sleep_until = start + elapsed;
-                let now = Instant::now();
-
-                if now < sleep_until {
-                    thread::sleep(sleep_until - now);
-                }
-
-                total_samples += sample_count as u64;
             }
             Ok(StreamRead::Metadata(_)) => {
                 // ignore metadata for now
