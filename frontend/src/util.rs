75cb7a54f33b6f772655c0367e3c27394a063ff1
Hailey Somerville <hailey@hailey.lol>
2020-08-04 15:45:50 +1000

    add util::origin

--- frontend/src/util.rs
+++ frontend/src/util.rs
@@ -54,6 +54,13 @@ impl Sequence {
     }
 }
 
+pub fn origin() -> String {
+    let location = web_sys::window().unwrap().location();
+    let proto = location.protocol().unwrap();
+    let host = location.host().unwrap();
+    format!("{}//{}", proto, host)
+}
+
 pub fn websocket_origin() -> String {
     let location = web_sys::window().unwrap().location();
     let proto = location.protocol().unwrap();
